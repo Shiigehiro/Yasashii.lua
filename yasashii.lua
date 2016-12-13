194c1638cb6e361460306f37bb3e46ae0f4d7f76
Shigehiro <Shiigehiro@users.noreply.github.com>
2016-12-13 16:37:34 +0100

    Update yasashii.lua

--- yasashii.lua
+++ yasashii.lua
@@ -12,9 +12,9 @@ client:on('messageCreate', function(msg)
   if (msg.author.bot == true) then return true end
   if (msg.channel.isPrivate == true) then return true end
     
-  if (string.find(msg.content, ".", 1, 1)) then
+  if (string.find(msg.content, ".", 1, 1)) then 
     local cmd = string.sub(string.match(msg.content, '(%S+) (.*)'), string.len(prefix)+1)
-		local args = string.sub(msg.content, string.len(cmd)+2))
+    local args = string.sub(msg.content, string.len(cmd)+2))
   end
 end)
 

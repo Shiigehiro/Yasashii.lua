63fbe35281f1549d8d0af9eb53c0330900cc055a
Shigehiro <Shiigehiro@users.noreply.github.com>
2016-12-14 14:42:43 +0100

    Update yasashii.lua

--- yasashii.lua
+++ yasashii.lua
@@ -1,14 +1,26 @@
-
+-- Modules -- 
 local discordia = require('discordia')
 local general = require('./commands/')
 local prefix = "."
 
+-- Global vars --
 _G.client = discordia.Client()
 
+-- Function --
+function read_file(path)
+	local file = io.open(path, "rb")
+	if not file then return nil end
+	local content = file:read "*a"
+	file:close()
+	return content
+end
+
+-- Discordia Event : Ready --
 client:on('ready', function()
   print(clien.userCount.." users, in "..client.channelCount.." channels of "..client.guildCount.."servers")
 end)
 
+-- Discordia Event : MessageCreate --
 client:on('messageCreate', function(msg)
   if (msg.author.bot == true) then return true end
   if (msg.channel.isPrivate == true) then return true end
@@ -18,7 +30,7 @@ client:on('messageCreate', function(msg)
     local args = string.sub(msg.content, string.len(cmd)+2))
     local cmds = read_file("./commands/"..cmd..".lua")
     if cmds ~= "" and cmds ~= nil then
-      print( "X must be equal to 1 or 0" )
+      print('Work !)
       return  
     end
   end
